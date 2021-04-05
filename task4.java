0e9a74dd387876651551cd9e525afe889eb152bf
AashishChilwal <78070029+AashishChilwal@users.noreply.github.com>
2021-04-05 17:40:11 -0600

    Update task4.java

--- task4.java
+++ task4.java
@@ -1,1 +1,39 @@
+import java.util.Scanner;
+
+public class task4 {
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in); 
+		System.out.print("Enter a password: ");
+		String password = input.nextLine();
+
+	   if (isValidPassword(password) == true)
+	    System.out.println("Valid Password");
+	   else
+	    System.out.println("Invalid Password");
+	}
+
+	public static boolean isValidPassword(String password) {
+		int length = 8;	
+	    int number = 0;
+	    boolean lettandnum = true ;
+        for(int i = 0;i<password.length();i++){
+            if (Character.isDigit(password.charAt(i))){
+                number ++;
+            }
+            else 
+            {
+            }
+        if ((password.charAt(i) >= 48 && password.charAt(i) <= 57) && (password.charAt(i) >= 97 && password.charAt(i) <= 122))
+        {
+            if (number== 0) {
+            lettandnum = true;
+            }
+        }
+        else{
+            lettandnum = false;
+        }
+        }
+          return lettandnum;
+	}
+}
 
