49ab9124f3ae58c15183e617e98fa4cca3696529
AashishChilwal <78070029+AashishChilwal@users.noreply.github.com>
2021-04-05 17:02:50 -0600

    Done with task 3

--- task3.java
+++ task3.java
@@ -1,1 +1,16 @@
-
+public class task3 {
+	public static void main(String[] args) {
+		for (int year = 2000; year <= 2010; year++) {
+			System.out.println(year + " has " + numberOfDaysInAYear(year));
+		}
+	}
+		public static boolean isLeapYear(int year) {
+		return (year % 4 == 0&&year % 100 != 0) || (year % 400 == 0);
+	}
+	public static int numberOfDaysInAYear(int year){
+		if (isLeapYear(year) == true)
+			return 366;
+		else
+			return 365;
+	}
+}
